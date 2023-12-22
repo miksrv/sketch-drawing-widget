58761b3befacfe0eb96f6f21c925f0ae39eea643
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2023-12-22 14:46:56 -0800

    Added ESLint and Prettier

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,23 +1,42 @@
+/**
+ * Button component for user interaction.
+ *
+ * @module Button
+ */
+
 import React from "react";
 
-import styles from './styles.module.sass'
+import styles from './styles.module.sass';
+
+/**
+ * @typedef {Object} InputProps - Props for the Button component.
+ * @property {string} [children] - The content inside the button.
+ * @property {'primary' | 'default'} [variant] - The variant style of the button.
+ * @property {boolean} [disabled] - Whether the button is disabled.
+ */
 
 interface InputProps extends React.ButtonHTMLAttributes<any> {
-    children?: string
-    variant?: 'primary' | 'default'
+    children?: string;
+    variant?: 'primary' | 'default';
 }
 
-const Button: React.FC<InputProps> = (props) => (
+/**
+ * Button component that renders a clickable button with specified styles.
+ *
+ * @param {InputProps} props - Props passed to the Button component.
+ * @returns {JSX.Element} Returns the JSX element of the Button component.
+ */
+const Button: React.FC<InputProps> = (props: InputProps): JSX.Element => (
     <button
         type={'button'}
         className={`
             ${styles.component}
-            ${props.disabled ? styles.disabled : undefined}
-            ${props.variant  === 'primary' ? styles.primary : styles.default}
+            ${props.disabled ? styles.disabled : ''}
+            ${props.variant === 'primary' ? styles.primary : styles.default}
         `}
     >
         {props.children}
     </button>
-)
+);
 
-export default Button
+export default Button;
