0bb23da9a14446dc7964b858db73192a43f65a56
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2023-12-24 22:56:29 -0800

    Added typedoc for Input UI component

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -2,11 +2,24 @@ import React from 'react'
 
 import styles from './styles.module.sass'
 
+/**
+ * Props for the Input component.
+ */
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+    /**
+     * Optional label text for the input field.
+     */
     label?: string
 }
 
-const Input: React.FC<InputProps> = (props) => (
+/**
+ * Input component for text input.
+ *
+ * @component
+ * @param {InputProps} props - The component props.
+ * @return {JSX.Element} The Input component.
+ */
+const Input: React.FC<InputProps> = (props: InputProps): JSX.Element => (
     <div className={styles.component}>
         {props.label && <label className={styles.label}>{props.label}</label>}
         <span className={styles.formField}>
