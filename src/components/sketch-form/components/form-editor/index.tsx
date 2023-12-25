efd19f011cf8910c871e8a7dcb586c8331ffb7fb
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2023-12-24 22:57:14 -0800

    Added typedoc for FormEditor UI component

--- src/components/sketch-form/components/form-editor/index.tsx
+++ src/components/sketch-form/components/form-editor/index.tsx
@@ -5,16 +5,36 @@ import Input from '../../../input'
 import { FormProps } from '../../types'
 import styles from './styles.module.sass'
 
+/**
+ * Props for the FormEditor component.
+ */
 interface FormEditorProps {
+    /**
+     * Current state of the form.
+     */
     formState?: FormProps
+
+    /**
+     * Callback function triggered when the form state changes.
+     * @param {keyof FormProps} name - The name of the property being changed.
+     * @param {string} value - The new value for the property.
+     */
     onChangeFormState?: (name: keyof FormProps, value: string) => void
 }
 
+/**
+ * Component for editing form details.
+ *
+ * @component
+ * @param {FormEditorProps} props - The component props.
+ * @return {JSX.Element} The FormEditor component.
+ */
 const FormEditor: React.FC<FormEditorProps> = (props) => {
     const { formState, onChangeFormState } = props
 
     return (
         <div className={styles.section}>
+            {/* Input for the profile title */}
             <Input
                 value={formState?.title}
                 label={'Название профиля'}
@@ -23,6 +43,8 @@ const FormEditor: React.FC<FormEditorProps> = (props) => {
                     onChangeFormState?.('title', event.target.value)
                 }}
             />
+
+            {/* Input for the encoded profile coordinates */}
             <Input
                 value={encodeCoordinates(formState?.sketch || [])}
                 label={'Код профиля'}
