ef30106fcd52e2333dcfb4c46b5e262c9b2cf28c
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2023-12-24 22:58:02 -0800

    Added typedoc for Message UI component

--- src/components/sketch-form/components/message/index.tsx
+++ src/components/sketch-form/components/message/index.tsx
@@ -2,14 +2,37 @@ import React from 'react'
 
 import styles from './styles.module.sass'
 
+/**
+ * Props for the Message component.
+ */
 interface MessageProps {
+    /**
+     * The title of the message.
+     */
     title?: string
+
+    /**
+     * The content/body of the message.
+     */
     content?: string
 }
 
-const Message: React.FC<MessageProps> = ({ title, content }) => (
+/**
+ * Component to display a simple message with an optional title and content.
+ *
+ * @component
+ * @param {MessageProps} props - The component props.
+ * @return {JSX.Element} The Message component.
+ */
+const Message: React.FC<MessageProps> = ({
+    title,
+    content
+}: MessageProps): JSX.Element => (
     <div className={styles.message}>
+        {/* Display the title if provided */}
         {title && <h4 className={styles.title}>{title}</h4>}
+
+        {/* Display the content if provided */}
         {content && <p className={styles.content}>{content}</p>}
     </div>
 )
