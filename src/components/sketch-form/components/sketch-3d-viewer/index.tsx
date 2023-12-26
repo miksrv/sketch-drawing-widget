1fb1b3cd72c95d7cafa2e58ed31ed240ef417161
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2023-12-25 22:05:11 -0800

    Added metal texture

--- src/components/sketch-form/components/sketch-3d-viewer/index.tsx
+++ src/components/sketch-form/components/sketch-3d-viewer/index.tsx
@@ -1,105 +1,127 @@
 import * as THREE from 'three'
 import { OrbitControls, Text } from '@react-three/drei'
-import { Canvas } from '@react-three/fiber'
+import { Canvas, useLoader } from '@react-three/fiber'
 import { Point2D } from 'functions/types'
-import React, { useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+
+import imageTexture from './metal-texture.jpg'
 
 interface Sketch3DViewerProps {
     sketch?: Point2D[]
 }
 
+const height = 5
+const depth = 100
+
 const Sketch3DViewer: React.FC<Sketch3DViewerProps> = ({ sketch }) => {
     const ref = useRef<any>()
     const groupRef = useRef<any>()
 
-    const points2D = sketch || []
+    const [geometries, setGeometries] = useState<any>()
+    const [centerX, setCenterX] = useState<any>()
+    const [centerY, setCenterY] = useState<any>()
+    const [cameraDistance, setCameraDistance] = useState<any>()
+    const [rectangles, setRectangles] = useState<any>()
 
-    const height = 5
-    const depth = 100
+    const [texture, setTexture] = useState(null)
 
-    // const geometries = create3DModelFrom2D(points2D, height);
+    // Загрузка текстуры (пример для PNG-файла)
+    // const texture = useLoader(THREE.TextureLoader, imageTexture)
 
-    // Вычислите среднюю позицию объектов
-    const geometries = points2D.map(({ x, y }) => ({ x, y, z: height }))
+    useEffect(() => {
+        const loadTexture = async () => {
+            const loader = new THREE.TextureLoader()
+            const loadedTexture: any = await new Promise((resolve) => {
+                loader.load(imageTexture, (texture) => {
+                    resolve(texture)
+                })
+            })
 
-    // const averagePosition = calculateAveragePosition(geometries);
+            setTexture(loadedTexture)
+        }
 
-    // Находим минимальные и максимальные значения по осям X и Y
-    const minX = Math.min(...points2D.map((point) => point.x))
-    const maxX = Math.max(...points2D.map((point) => point.x))
-    const minY = Math.min(...points2D.map((point) => point.y))
-    const maxY = Math.max(...points2D.map((point) => point.y))
+        loadTexture()
+    }, [])
 
-    // Находим центр объекта
-    const centerX = (minX + maxX) / 2
-    const centerY = (minY + maxY) / 2
+    useEffect(() => {
+        const points2D = sketch || []
 
-    // Находим диагональ объекта
-    const diagonal = Math.sqrt((maxX - minX) ** 2 + (maxY - minY) ** 2)
+        // Вычислите среднюю позицию объектов
+        const geometries = points2D.map(({ x, y }) => ({ x, y, z: height }))
+        setGeometries(geometries)
 
-    // Вычисляем расстояние камеры от объекта
-    const cameraDistance = diagonal / (2 * Math.tan(Math.PI * 0.5 * 0.5))
+        // Находим минимальные и максимальные значения по осям X и Y
+        const minX = Math.min(...points2D.map((point) => point.x))
+        const maxX = Math.max(...points2D.map((point) => point.x))
+        const minY = Math.min(...points2D.map((point) => point.y))
+        const maxY = Math.max(...points2D.map((point) => point.y))
 
-    // Генерация 3D-сцены
-    // const lines = [];
-    // for (let i = 0; i < points2D.length - 1; i++) {
-    //     const start = new THREE.Vector3(points2D[i].x, points2D[i].y, 0);
-    //     const end = new THREE.Vector3(points2D[i + 1].x, points2D[i + 1].y, 0);
-    //
-    //     const geometry = new THREE.BufferGeometry().setFromPoints([start, end]);
-    //     const material = new THREE.LineBasicMaterial({ color: 0x00ff00 });
-    //
-    //     const line = <line key={i}>
-    //         <bufferGeometry attach="geometry" {...geometry} />
-    //         <lineBasicMaterial attach="material" {...material} />
-    //     </line>;
-    //
-    //     lines.push(line);
-    // }
+        // Находим центр объекта
+        setCenterX((minX + maxX) / 2)
+        setCenterY((minY + maxY) / 2)
 
-    const rectangles = []
-    for (let i = 0; i < points2D.length - 1; i++) {
-        const start = new THREE.Vector3(points2D[i].x, points2D[i].y, 0)
-        const end = new THREE.Vector3(points2D[i + 1].x, points2D[i + 1].y, 0)
+        // Находим диагональ объекта
+        const diagonal = Math.sqrt((maxX - minX) ** 2 + (maxY - minY) ** 2)
 
-        const length = start.distanceTo(end)
+        // Вычисляем расстояние камеры от объекта
+        const cameraDistance = diagonal / (2 * Math.tan(Math.PI * 0.5 * 0.5))
+        setCameraDistance(cameraDistance)
 
-        const quaternion = new THREE.Quaternion()
-        const direction = end.clone().sub(start).normalize()
-        quaternion.setFromUnitVectors(new THREE.Vector3(1, 0, 0), direction)
+        const rectangles = []
 
-        const geometry = new THREE.BoxGeometry(length, height, depth)
-        const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+        for (let i = 0; i < points2D.length - 1; i++) {
+            const start = new THREE.Vector3(points2D[i].x, points2D[i].y, 0)
+            const end = new THREE.Vector3(
+                points2D[i + 1].x,
+                points2D[i + 1].y,
+                0
+            )
 
-        const rectangle = (
-            <mesh
-                key={i}
-                position={start.clone().add(end).multiplyScalar(0.5)}
-                quaternion={quaternion}
-            >
-                <bufferGeometry
-                    attach='geometry'
-                    {...geometry}
-                />
-                <meshBasicMaterial
-                    attach='material'
-                    {...material}
-                />
-                <Text
-                    position={[0, 0, height + 50]}
-                    // rotation={quaternion}
-                    anchorX={'center'}
-                    anchorY={'middle'}
-                    fontSize={17}
-                    color={'#000000'}
+            const length = start.distanceTo(end)
+
+            const quaternion = new THREE.Quaternion()
+            const direction = end.clone().sub(start).normalize()
+            quaternion.setFromUnitVectors(new THREE.Vector3(1, 0, 0), direction)
+
+            const geometry = new THREE.BoxGeometry(length, height, depth)
+            // const material = new THREE.MeshStandardMaterial({ map: texture })
+            const material = new THREE.MeshBasicMaterial({
+                color: '#dcd9c9',
+                map: texture
+            })
+
+            const rectangle = (
+                <mesh
+                    key={i}
+                    position={start.clone().add(end).multiplyScalar(0.5)}
+                    quaternion={quaternion}
                 >
-                    {`${length.toFixed(0)} mm`}
-                </Text>
-            </mesh>
-        )
+                    <bufferGeometry
+                        attach='geometry'
+                        {...geometry}
+                    />
+                    <meshBasicMaterial
+                        {...material}
+                        attach={'material'}
+                        map={texture}
+                    />
+                    <Text
+                        position={[0, 0, height + 50]}
+                        anchorX={'center'}
+                        anchorY={'middle'}
+                        fontSize={17}
+                        color={'#000000'}
+                    >
+                        {`${length.toFixed(0)} mm`}
+                    </Text>
+                </mesh>
+            )
 
-        rectangles.push(rectangle)
-    }
+            rectangles.push(rectangle)
+        }
+
+        setRectangles(rectangles)
+    }, [sketch, texture])
 
     return (
         <Canvas
@@ -125,24 +147,30 @@ const Sketch3DViewer: React.FC<Sketch3DViewerProps> = ({ sketch }) => {
                 intensity={Math.PI}
             />
 
-            {/*{lines}*/}
-
             <group
                 ref={groupRef}
                 position={[centerY - 450, centerX - 450, 0]}
             >
                 {rectangles}
 
-                {geometries.map((geometry, index) => (
-                    <mesh
-                        key={index}
-                        ref={ref}
-                        position={[geometry.x, geometry.y, geometry.z - 5]}
-                    >
-                        <boxGeometry args={[height, height, depth]} />
-                        <meshStandardMaterial color='orange' />
-                    </mesh>
-                ))}
+                {geometries?.map(
+                    (
+                        geometry: { x: number; y: number; z: number },
+                        index: string | number | bigint | undefined
+                    ) => (
+                        <mesh
+                            key={index}
+                            ref={ref}
+                            position={[geometry.x, geometry.y, geometry.z - 5]}
+                        >
+                            <boxGeometry args={[height, height, depth]} />
+                            <meshBasicMaterial
+                                attach={'material'}
+                                map={texture}
+                            />
+                        </mesh>
+                    )
+                )}
             </group>
 
             <OrbitControls
