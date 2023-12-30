fcd53491ac0ce1270802dc572420d333e30b22e3
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2023-12-29 20:02:25 -0800

    Added sketch line length

--- src/components/sketch-form/components/sketch-2d-scan/index.tsx
+++ src/components/sketch-form/components/sketch-2d-scan/index.tsx
@@ -12,8 +12,8 @@ const Sketch2DScan: React.FC<Sketch2DScanProps> = ({ sketch }) => {
         const canvas = canvasRef.current
         if (!canvas || !sketch?.length) return
 
-        const newPoints = calculateNewPoints(sketch)
-        const allPoints = addShiftedPoints(newPoints)
+        const firstVertical = calculatefirstVertical(sketch)
+        const lastVertical = addShiftedPoints(firstVertical)
 
         const ctx = canvas.getContext('2d')
         if (!ctx) return
@@ -23,37 +23,69 @@ const Sketch2DScan: React.FC<Sketch2DScanProps> = ({ sketch }) => {
 
         // Начало отрисовки
         ctx.beginPath()
-        ctx.moveTo(newPoints?.[0].x, newPoints?.[0].y) // Начинаем с первой точки
+        ctx.moveTo(firstVertical?.[0].x, firstVertical?.[0].y) // Начинаем с первой точки
 
-        for (let i = 0; i < newPoints.length - 1; i += 1) {
-            ctx.moveTo(newPoints[i].x, newPoints[i].y) // Устанавливаем начальную точку
-            ctx.lineTo(newPoints[i + 1].x, newPoints[i + 1].y) // Рисуем линию к следующей точке
+        for (let i = 0; i < firstVertical.length - 1; i += 1) {
+            const startX = firstVertical[i].x
+            const startY = firstVertical[i].y
+            const endX = firstVertical[i + 1].x
+            const endY = firstVertical[i + 1].y
+
+            // Рисуем линию
+            ctx.moveTo(startX, startY)
+            ctx.lineTo(endX, endY)
+            ctx.stroke()
+
+            // Вычисляем длину линии (предполагая, что это ваши координаты в пикселях и 1 единица в пикселях = 1 мм)
+            const lengthInPixels = Math.sqrt(
+                (sketch[i + 1].x - sketch[i].x) ** 2 +
+                    (sketch[i + 1].y - sketch[i].y) ** 2
+            )
+
+            // Вычисляем середину линии
+            const midX = (startX + endX) / 2
+            const midY = (startY + endY) / 2
+
+            // Повернем текст на 90 градусов влево
+            ctx.save() // сохраняем текущее состояние контекста
+            ctx.translate(midX, midY) // перемещаем начало координат в середину линии
+            ctx.rotate(-Math.PI / 2) // поворачиваем на 90 градусов против часовой стрелки
+
+            ctx.fillStyle = 'black' // Цвет текста
+            ctx.font = '12px Arial' // Шрифт и размер
+            ctx.textAlign = 'center' // Выравнивание по центру
+            ctx.fillText(`${Math.round(lengthInPixels)} мм`, 0, -5) // Отображаем текст
+
+            ctx.restore() // восстанавливаем исходное состояние контекста
+        }
+
+        for (let i = 0; i < lastVertical.length - 1; i += 1) {
+            ctx.moveTo(lastVertical[i].x, lastVertical[i].y) // Устанавливаем начальную точку
+            ctx.lineTo(lastVertical[i + 1].x, lastVertical[i + 1].y) // Рисуем линию к следующей точке
             ctx.stroke()
         }
 
-        for (let i = 0; i < allPoints.length - 1; i += 1) {
-            ctx.moveTo(allPoints[i].x, allPoints[i].y) // Устанавливаем начальную точку
-            ctx.lineTo(allPoints[i + 1].x, allPoints[i + 1].y) // Рисуем линию к следующей точке
-            ctx.stroke()
-        }
-
-        for (let i = 0; i < Math.min(newPoints.length, allPoints.length); i++) {
+        for (
+            let i = 0;
+            i < Math.min(firstVertical.length, lastVertical.length);
+            i++
+        ) {
             // Если это первая или последняя точка, рисуем сплошную линию
             if (
                 i === 0 ||
-                i === Math.min(newPoints.length, allPoints.length) - 1
+                i === Math.min(firstVertical.length, lastVertical.length) - 1
             ) {
                 ctx.setLineDash([]) // Сбросить пунктирный стиль
             } else {
-                ctx.setLineDash([5, 5]) // Установить пунктирный стиль
+                ctx.setLineDash([4, 4]) // Установить пунктирный стиль
             }
 
             // Начинаем новый путь
             ctx.beginPath()
 
             // Рисуем горизонтальную линию между точками
-            ctx.moveTo(newPoints[i].x, newPoints[i].y) // Начало линии из newPoints
-            ctx.lineTo(allPoints[i].x, allPoints[i].y) // Конец линии в allPoints
+            ctx.moveTo(firstVertical[i].x, firstVertical[i].y) // Начало линии из firstVertical
+            ctx.lineTo(lastVertical[i].x, lastVertical[i].y) // Конец линии в lastVertical
 
             // Заканчиваем путь и рисуем текущую линию
             ctx.stroke()
@@ -72,7 +104,7 @@ const Sketch2DScan: React.FC<Sketch2DScanProps> = ({ sketch }) => {
     )
 }
 
-const calculateNewPoints = (originalPoints: Point2D[]): Point2D[] => {
+const calculatefirstVertical = (originalPoints: Point2D[]): Point2D[] => {
     // 1. Рассчитать расстояние между каждой точкой в исходном массиве
     const distances: number[] = []
     for (let i = 0; i < originalPoints.length - 1; i++) {
@@ -92,16 +124,16 @@ const calculateNewPoints = (originalPoints: Point2D[]): Point2D[] => {
     )
 
     // 4. Создать новый массив точек с учетом пропорций
-    const newPoints: Point2D[] = [{ x: 100, y: 50 }]
+    const firstVertical: Point2D[] = [{ x: 100, y: 50 }]
 
     const numbersFrom350 = percentages.map((percent) => percent * 3)
 
     for (let i = 0; i < numbersFrom350.length; i++) {
-        const newY = newPoints[i].y + numbersFrom350[i] // рассчитываем новую координату y
-        newPoints.push({ x: 100, y: newY }) // добавляем новую точку в массив
+        const newY = firstVertical[i].y + numbersFrom350[i] // рассчитываем новую координату y
+        firstVertical.push({ x: 100, y: newY }) // добавляем новую точку в массив
     }
 
-    return newPoints
+    return firstVertical
 }
 
 const addShiftedPoints = (originalPoints: Point2D[]) =>
